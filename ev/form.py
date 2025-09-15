eca52f84cee30b1d695708b42eb76c0bd880787f
Sakib3602 <sakibsarker6969@gmail.com>
2025-09-15 12:18:30 +0600

    forms make using model.forms

--- ev/form.py
+++ ev/form.py
@@ -1,1 +1,19 @@
 from django import forms
+from ev.models import Event, Participent, Category
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ["name", "description", "date", "time", "location", "category"]
+        widgets = {
+            "date" : forms.DateField(widget=forms.SelectDateWidget()),
+            "time" : forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'})) 
+        }
+class Participent(forms.ModelForm):
+    class Meta:
+        model = Participent
+        fields = ["name", "email", "events"]
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name", "description"]
--- ev/views.py
+++ ev/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "banner.html")
+
+
+
+def AddEvent(request):
+    return render(request, "add_event.html")
